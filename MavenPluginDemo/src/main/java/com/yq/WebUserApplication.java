4edfb18b35e8b68cb981ab6765b8a3d05d0f5abf
yqbjtu <yqbjtu@163.com>
2018-08-19 20:06:38 +0800

    add maven-dependency-plugin demo

---
+++ MavenPluginDemo/src/main/java/com/yq/WebUserApplication.java
@@ -0,0 +1,13 @@
+package com.yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUserApplication.class, args);
+	}
+}
