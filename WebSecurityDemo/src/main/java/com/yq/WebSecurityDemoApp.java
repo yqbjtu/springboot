c6cc2e06cff7ebcd6ac597ccf32d45f3e066ed3f
yqbjtu <yqbjtu@163.com>
2018-09-08 16:10:56 +0800

    websecurity demo

---
+++ WebSecurityDemo/src/main/java/com/yq/WebSecurityDemoApp.java
@@ -0,0 +1,23 @@
+package com.yq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication(scanBasePackages = {"com.yq"})
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
+public class WebSecurityDemoApp {
+    private static final Logger log = LoggerFactory.getLogger(WebSecurityDemoApp.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSecurityDemoApp.class, args);
+    }
+
+}
---
+++ WebSecurityDemo/webSecurityDemo_readme.md
@@ -0,0 +1,16 @@
+#WebSecurity
+
+
+1, http://127.0.0.1:6060/swagger-ui.html  
+
+2. http://127.0.0.1:6060/hello   get request  
+
+
+```
+
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-security</artifactId>
+        </dependency>
+
+```
