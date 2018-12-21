ecc51cc4b2f69ed80dcb7e21cf341cc9c1e1a0f8
yqbjtu <yqbjtu@163.com>
2018-12-21 19:24:54 +0800

    a simple akka demo

---
+++ AkkaDemo/src/main/java/com/yq/AkkaAlarmDemo.java
@@ -0,0 +1,44 @@
+package com.yq;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import com.yq.actor.ClearAlarmAction;
+import com.yq.actor.CreateAlarmAction;
+import com.yq.actor.FilterScript;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AkkaAlarmDemo {
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("AlarmDemo");
+        try {
+
+            final ActorRef createAlarmActor =
+                    system.actorOf(CreateAlarmAction.props(), "createAlarmActor");
+            final ActorRef clearAlarmActor =
+                    system.actorOf(ClearAlarmAction.props(), "clearAlarmActor");
+            final ActorRef filterScript =
+                    system.actorOf(FilterScript.props("filterScriptActor", createAlarmActor, clearAlarmActor), "filterScript");
+
+
+            //#create-actors
+
+            //#main-send-messages
+            Map<String, Object> sensorDataMap = new HashMap<>();
+            sensorDataMap.put("temperature", 90);
+
+            FilterScript.DeviceDataEvent deviceDataAndRule = new FilterScript.DeviceDataEvent("device001", sensorDataMap);
+            filterScript.tell(deviceDataAndRule, ActorRef.noSender());
+
+
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+        } catch (IOException ioe) {
+        } finally {
+            system.terminate();
+        }
+    }
+}
---
+++ AkkaDemo/src/main/java/com/yq/actor/CreateAlarmAction.java
@@ -0,0 +1,46 @@
+package com.yq.actor;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+
+public class CreateAlarmAction extends AbstractActor {
+
+    static public Props props() {
+        return Props.create(CreateAlarmAction.class, () -> new CreateAlarmAction());
+    }
+
+    static public class AlarmMessage {
+        public final String deviceId;
+        public final String deviceName;
+
+        public AlarmMessage(String deviceId, String deviceName) {
+            this.deviceId = deviceId;
+            this.deviceName = deviceName;
+        }
+    }
+
+
+    private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    public CreateAlarmAction() {
+    }
+
+    /*
+    The createReceive method should handle the messages the actor expects.
+    In the case of Greeter, it expects two types of messages: DeviceDataEvent and Greet.
+     */
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(AlarmMessage.class, greeting -> {
+                    long threadId = Thread.currentThread().getId();
+                    log.info("deviceName={}, threadId={}, create an alarm if not created", greeting.deviceName ,threadId);
+                })
+                .build();
+    }
+
+}
+
