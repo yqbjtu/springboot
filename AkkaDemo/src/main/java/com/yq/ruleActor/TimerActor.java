8b21303077838f7a1d190c854d6e788852b690ec
yqbjtu <yqbjtu@163.com>
2019-01-09 11:09:18 +0800

    add a scheduler timer actor

---
+++ AkkaDemo/src/main/java/com/yq/ruleActor/TimerActor.java
@@ -0,0 +1,50 @@
+package com.yq.ruleActor;
+
+import akka.actor.AbstractActorWithTimers;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import com.yq.context.IoTContext;
+
+import java.time.Duration;
+
+/**
+ * Simple to Introduction
+ * className: TimerActor
+ *
+ * @author EricYang
+ * @version 2019/1/9 10:59
+ */
+
+public class TimerActor extends AbstractActorWithTimers {
+    private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    private static Object TICK_KEY = "TickKey";
+    public static final class FirstTick {
+    }
+    public static final class Tick {
+    }
+
+    public TimerActor() {
+        getTimers().startSingleTimer(TICK_KEY, new FirstTick(), Duration.ofMillis(500));
+    }
+
+    static public Props props() {
+        return Props.create(TimerActor.class, () -> new TimerActor());
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(FirstTick.class, message -> {
+                    // do something useful here
+                    log.info("FirstTick");
+                    getTimers().startPeriodicTimer(TICK_KEY, new Tick(), Duration.ofSeconds(10));
+                })
+                .match(Tick.class, message -> {
+                    // do something useful here
+                    log.info("Tick");
+                })
+                .build();
+    }
+}
